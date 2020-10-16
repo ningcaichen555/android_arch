803ddcbad8beea876dc78c9877cb7c1797d74d36
caichen <looksanjin@gmail.com>
2020-10-16 18:08:54 +0800

    canvas path

---
+++ viewapp/src/main/java/com/example/viewapp/BezierView.java
@@ -0,0 +1,129 @@
+package com.example.viewapp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+class BezierView extends View {
+    private Paint mPaint;
+    private PointF start, end, control;
+    private float centerX, centerY;
+    private GestureDetector gestureDetector;
+
+    public BezierView(Context context) {
+        this(context, null);
+    }
+
+    public BezierView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BezierView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initPaint();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(8);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setTextSize(60);
+
+        start = new PointF(0, 0);
+        end = new PointF(0, 0);
+        control = new PointF(0, 0);
+
+        gestureDetector = new GestureDetector(getContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                float x = e2.getX();
+                float y = e2.getY();
+                control.x = x;
+                control.y = y;
+                invalidate();
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerX = w / 2;
+        centerY = h / 2;
+        start.x = centerX - 200;
+        start.y = centerY;
+
+        end.x = centerX + 200;
+        end.y = centerY;
+
+        control.x = centerX;
+        control.y = centerY - 200;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPaint.setColor(Color.GRAY);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStrokeWidth(40);
+
+        canvas.drawPoint(start.x, start.y, mPaint);
+        canvas.drawPoint(end.x, end.y, mPaint);
+        canvas.drawPoint(control.x, control.y, mPaint);
+
+        mPaint.setStrokeWidth(10);
+        mPaint.setColor(Color.RED);
+        canvas.drawLine(start.x, start.y, control.x, control.y, mPaint);
+        canvas.drawLine(end.x, end.y, control.x, control.y, mPaint);
+
+//        //绘制贝塞尔曲线
+        mPaint.setColor(Color.GREEN);
+        Path path = new Path();
+        path.moveTo(start.x, start.y);
+        path.quadTo(control.x, control.y, end.x, end.y);
+        canvas.drawPath(path, mPaint);
+    }
+}
--- viewapp/src/main/java/com/example/viewapp/CanvasView.java
+++ viewapp/src/main/java/com/example/viewapp/CanvasView.java
@@ -27,6 +27,7 @@ public class CanvasView extends View {
     private Picture mPicture;
 
     private RectF arcRect;
+    private Paint textPaint;
 
     public CanvasView(Context context) {
         this(context, null);
@@ -66,6 +67,11 @@ public class CanvasView extends View {
         mPicture = new Picture();
 
         arcRect = new RectF(screenUtil.dip2px(60f), screenUtil.dip2px(250f), screenUtil.dip2px(90f), screenUtil.dip2px(280f));
+
+        textPaint = new Paint();
+        textPaint.setColor(Color.argb(255, 22, 222, 222));
+        textPaint.setTextSize(screenUtil.dip2px(40f));
+        textPaint.setStyle(Paint.Style.FILL);
     }
 
 
@@ -112,38 +118,56 @@ public class CanvasView extends View {
         canvas.restore();
 
 
-        mPicture.beginRecording(canvas.getWidth(), canvas.getHeight());
+        Canvas canvasPic = mPicture.beginRecording(canvas.getWidth(), canvas.getHeight());
 
-        canvas.drawArc(arcRect, 0f, 90, true, arcPaint);
+        canvasPic.drawArc(arcRect, 0f, 90, true, arcPaint);
 
-        canvas.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(400f), screenUtil.dip2px(100f), screenUtil.dip2px(400f), arcPaint);
+        canvasPic.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(400f), screenUtil.dip2px(100f), screenUtil.dip2px(400f), arcPaint);
 
-        canvas.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
+        canvasPic.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
 
-        canvas.drawArc(arcRect, 0f, 90, true, arcPaint);
+        canvasPic.drawArc(arcRect, 0f, 90, true, arcPaint);
 
-        canvas.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(420f), screenUtil.dip2px(100f), screenUtil.dip2px(420f), arcPaint);
+        canvasPic.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(400f), screenUtil.dip2px(100f), screenUtil.dip2px(400f), arcPaint);
 
-        canvas.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
+        canvasPic.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
 
-        canvas.drawArc(arcRect, 0f, 90, true, arcPaint);
+        canvasPic.drawArc(arcRect, 0f, 90, true, arcPaint);
 
-        canvas.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(440f), screenUtil.dip2px(100f), screenUtil.dip2px(440f), arcPaint);
+        canvasPic.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(400f), screenUtil.dip2px(100f), screenUtil.dip2px(400f), arcPaint);
 
-        canvas.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
+        canvasPic.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
 
-        canvas.drawArc(arcRect, 0f, 90, true, arcPaint);
+        canvasPic.drawArc(arcRect, 0f, 90, true, arcPaint);
 
-        canvas.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(460f), screenUtil.dip2px(100f), screenUtil.dip2px(460f), arcPaint);
+        canvasPic.drawLine(screenUtil.dip2px(30f), screenUtil.dip2px(400f), screenUtil.dip2px(100f), screenUtil.dip2px(400f), arcPaint);
 
-        canvas.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
+        canvasPic.rotate(90, canvas.getWidth() / 2, canvas.getHeight() / 2);
 
         mPicture.endRecording();
 
-        canvas.scale(0.5f, 0.5f);
-
         canvas.drawPicture(mPicture);
 
+//
+//        第一类 只能指定文本基线位置(基线x默认在字符串左侧，基线y默认在字符串下方)。
+//        public void drawText (String text, float x, float y, Paint paint)
+//        public void drawText (String text, int start, int end, float x, float y, Paint paint)
+//        public void drawText (CharSequence text, int start, int end, float x, float y, Paint paint)
+//        public void drawText (char[] text, int index, int count, float x, float y, Paint paint)
+//
+//        第二类  第二类可以分别指定每个文字的位置。
+//        public void drawPosText (String text, float[] pos, Paint paint)
+//        public void drawPosText (char[] text, int index, int count, float[] pos, Paint paint)
+//
+//        第三类  第三类是指定一个路径，根据路径绘制文字。
+//        public void drawTextOnPath (String text, Path path, float hOffset, float vOffset, Paint paint)
+//        public void drawTextOnPath (char[] text, int index, int count, Path path, float hOffset, float vOffset, Paint paint)
+
+        //以画布中心为中心进行缩放
+//        canvas.scale(0.5f, 0.5f, (float) canvas.getWidth() / 2, (float) canvas.getHeight() / 2);
+
+        String text = "abcdef";
+        canvas.drawText(text, 0, screenUtil.dip2px(30f), textPaint);
     }
 
     @Override
