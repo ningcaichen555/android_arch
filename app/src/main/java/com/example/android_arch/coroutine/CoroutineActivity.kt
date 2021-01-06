de32a6c29a258e26ded05e323df904b833351eca
caichen <looksanjin@gmail.com>
2021-01-06 14:31:45 +0800

    coroutine

---
+++ app/src/main/java/com/example/android_arch/coroutine/CoroutineActivity.kt
@@ -0,0 +1,38 @@
+package com.example.android_arch.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_arch.R
+import kotlinx.coroutines.*
+import utils.LogUtils
+
+class CoroutineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coroutine)
+        startCoroutine()
+        LogUtils.d("main")
+    }
+
+    val funTest: suspend CoroutineScope.() -> Unit = {
+        LogUtils.d("funTest")
+        suspendFun1()
+        suspendFun2()
+    }
+
+
+    fun startCoroutine() {
+        GlobalScope.launch(Dispatchers.Default, start = CoroutineStart.LAZY, block = funTest)
+    }
+
+    // 挂起函数
+    suspend fun suspendFun1() {
+        LogUtils.d("suspendFun1")
+    }
+
+    // 挂起函数
+    suspend fun suspendFun2() {
+        LogUtils.d("suspendFun2")
+    }
+
+}
--- app/src/main/java/com/example/android_arch/data_bind/DataBindActivity.kt
+++ app/src/main/java/com/example/android_arch/data_bind/DataBindActivity.kt
@@ -2,16 +2,21 @@ package com.example.android_arch.data_bind
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil.setContentView
 import com.example.android_arch.R
 import com.example.android_arch.databinding.ActivityDataBindBinding
 
 class DataBindActivity : AppCompatActivity() {
+    val dataBindViewModel = DataBindViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = setContentView<ActivityDataBindBinding>(this, R.layout.activity_data_bind)
-        val dataBindViewModel = DataBindViewModel()
         binding.dataBindModel = dataBindViewModel
         dataBindViewModel.name = "张三"
+    }
+
+    fun click_to_changName(view: View) {
+        dataBindViewModel.name = "李四"
     }
 }
