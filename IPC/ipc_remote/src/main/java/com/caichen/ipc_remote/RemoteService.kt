ec161c9f516af0734de4db000f7860d743a136be
zoux2 <zoux2@ziroom.com>
2022-06-15 10:05:07 +0800

    部分代码

--- IPC/ipc_remote/src/main/java/com/caichen/ipc_remote/RemoteService.kt
+++ IPC/ipc_remote/src/main/java/com/caichen/ipc_remote/RemoteService.kt
@@ -9,18 +9,16 @@ import android.widget.Toast
  * 提供子进程的链接和消息服务
  */
 class RemoteService : Service() {
-    var connected: Boolean = false
+    private var connected: Boolean = false
 
-    val connectService by lazy {
+    private val connectService by lazy {
         object : IConnectionService.Stub() {
             override fun connect() {
                 connected = true
-                Thread.sleep(5000)
             }
 
             override fun disConnect() {
                 connected = false
-                Toast.makeText(this@RemoteService, "disconnect", Toast.LENGTH_SHORT).show()
             }
 
             override fun isConnected(): Boolean {
